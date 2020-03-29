521a4486940037d81a30242ecbda3779cc64dc89
unknown <jasht074@uottawa.ca>
2020-03-29 11:30:26 -0400

    fixing temp/humidity sensor code

--- sensor_code/Temp-Humidity.py
+++ sensor_code/Temp-Humidity.py
@@ -13,13 +13,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 GPIO.setwarnings(False)
 
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+# Sensor is Adafruit_DHT.DHT11,
 sensor = Adafruit_DHT.DHT11
 
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
-pin = 4
+# connected to pin pi pin 16 which is GPIO23.
+pin = 23
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -27,6 +25,8 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 
 firebase = firebase.FirebaseApplication('https://capstonepi-e514c.firebaseio.com/', None)
+
+#database test code
 #firebase.put("/dht", "/temp", "0.00")
 #firebase.put("/dht", "/humidity", "0.00")
 
